bbad0b45dab41f6afe75cf97a599c40dd888fc2a
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-11 01:46:44 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // Please write your code here.
+    int xxxx,yyyy,a;
+    char b,c;
+    cin>>b;
+    cin>>a;
+    cin.get();
+    cin>>xxxx>>c>>yyyy;
+    cout<<b<<a<<c<<yyyy<<c<<xxxx;
+
+    return 0;
+}

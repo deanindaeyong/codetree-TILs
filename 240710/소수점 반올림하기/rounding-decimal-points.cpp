cec89182fa6bc16760d9267144c04b9a10b27e23
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-10 23:45:46 +0900

    Title: 소수점 반올림하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240710/소수점 반올림하기/rounding-decimal-points.cpp
+++ 240710/소수점 반올림하기/rounding-decimal-points.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main() {
     // Please write your code here.
     double a=25.352;
-    cout.precision(3);
+    cout<<fixed;
+    cout.precision(1);
     cout<<a;
     return 0;
 }

2f681efeeff901e4772a8778fd60e78700601990
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-11 01:38:46 +0900

    Title: 날짜 변경하여 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int mm,dd,yy;
+    cin>>yy;
+    cin.get();
+    cin>>mm;
+    cin.get();
+    cin>>dd;
+    cout<<mm<<"-"<<dd<<"-"<<yy;
+    return 0;
+}

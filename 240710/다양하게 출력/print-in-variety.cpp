ad2543f63eae6fd45b054c865bedd0d0ebcb47bd
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-10 23:15:01 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/다양하게 출력/print-in-variety.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int day=365;
+    double pi_2= 3.1415926535;
+
+    cout<<"Total days in Year"<<endl<<day<<endl<<"Circumference rate"<<endl;
+    cout.precision(11);
+    cout<<pi_2;
+    return 0;
+}

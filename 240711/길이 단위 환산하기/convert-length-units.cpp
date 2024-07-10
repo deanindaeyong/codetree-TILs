b39cd535b08984ccb131ae82fb1fee69959e5e3b
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-11 00:48:34 +0900

    Title: 길이 단위 환산하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240711/길이 단위 환산하기/convert-length-units.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    double n;
+    cin>>n;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<n*30.48;
+    return 0;
+}

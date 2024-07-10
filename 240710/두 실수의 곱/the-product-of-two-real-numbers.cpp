a96efc31b7578868514190b05fced0ef7ad702a6
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-10 23:51:21 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240710/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout<<fixed;
+    cout.precision(3);
+    double a=5.26,b=8.27;
+    cout<<a*b;
+    return 0;
+}

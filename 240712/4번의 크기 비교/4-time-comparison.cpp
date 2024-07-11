647d8c97bfba4c22225728e5d62428bbfc18d62e
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-12 02:16:10 +0900

    Title: 4번의 크기 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240712/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,c,d,e;
+    cin>>a>>b>>c>>d>>e;
+
+    if(a>b)cout<<1;
+    else cout<<0;
+    cout<<endl;
+    if(a>c)cout<<1;
+    else cout<<0;
+    cout<<endl;
+    if(a>d)cout<<1;
+    else cout<<0;
+    cout<<endl;
+    if(a>e)cout<<1;
+    else cout<<0;
+    
+    return 0;
+}

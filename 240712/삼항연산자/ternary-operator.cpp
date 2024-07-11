99ba28c4313060258a30522109792a248f34e489
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-12 00:44:07 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240712/삼항연산자/ternary-operator.cpp
+++ 240712/삼항연산자/ternary-operator.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 using namespace std;
-#include <string>
+
 int main() {
     // Please write your code here.
     int a;
     cin>>a;
 
-    string x=a==100?"pass":"failure";
-    cout<<x;
+    cout<<(a==100?"pass":"failure");
+    
     return 0;
 }

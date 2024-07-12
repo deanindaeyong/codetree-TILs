aab302769cdcfcf65b090141a2d2e26aab8e054e
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-13 01:34:13 +0900

    Title: 일 수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240713/일 수 구하기/number-of-days-in-month.cpp
+++ 240713/일 수 구하기/number-of-days-in-month.cpp
@@ -8,8 +8,8 @@ int main() {
 
     if(n==2)
     cout<<28;
-    else if(n==4||n==6||n==9||n==1)
-    cout<<31;
-    else cout<<30;
+    else if(n==4||n==6||n==9||n==11)
+    cout<<30;
+    else cout<<31;
     return 0;
 }

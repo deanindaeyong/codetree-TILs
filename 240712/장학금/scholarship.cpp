d5317429f7543e34513012a55f8d60241e0ead3c
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-12 23:49:18 +0900

    Title: 장학금, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240712/장학금/scholarship.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m,f;
+    cin>>m>>f;
+    if(m>90 && f>=95)
+    cout<<100000;
+    else if(m>90 &&f>=90)
+    cout<<50000;
+    else cout<<0;
+    return 0;
+}

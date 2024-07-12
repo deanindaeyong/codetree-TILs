46477d0536c23abeb80003c1b7247d9b5fe8d108
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-13 01:54:51 +0900

    Title: 코로나 메뉴얼, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240713/코로나 메뉴얼/covid-manual.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char pa,pb,pc;
+    int a1,b1,c1;
+    int a,b,c,d;
+    a=b=c=d=0;
+
+    cin>>pa>>a1>>pb>>b1>>pc>>c1;
+
+    if((pa=='Y'&& a1>37))
+        if((pb=='Y'&&b1>37)||(pc=='Y'&&c1>37))
+            cout<<'E';
+        else cout<<'N';
+    
+    else if((pb=='Y'&&b1>37)&&(pc=='Y'&&c1>37))
+    cout<<'E';
+    else cout<<'N';
+
+    return 0;
+}

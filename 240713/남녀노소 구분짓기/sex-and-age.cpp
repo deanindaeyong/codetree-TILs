8b33c1cf2fb349c570b1aa080e9065335ea56193
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-13 00:35:29 +0900

    Title: 남녀노소 구분짓기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240713/남녀노소 구분짓기/sex-and-age.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int m,a;
+    cin>>m>>a;
+
+    if(m==0){
+        if(a>=19)
+        cout<<"MAN";
+        else cout<<"BOY";
+    }
+    else{
+        if(a>=19)
+        cout<<"WOMAN";
+        else cout<<"GIRL";
+    }
+
+    return 0;
+}

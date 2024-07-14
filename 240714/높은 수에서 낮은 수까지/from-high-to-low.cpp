41f4d2073e72183b5ed1addf5acf386fd9b3dfcc
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 15:34:44 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240714/높은 수에서 낮은 수까지/from-high-to-low.cpp
+++ 240714/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -5,7 +5,11 @@ int main() {
     // Please write your code here.
     int a,b;
     cin>>a>>b;
+    if(b>a)
     for(int i=b;i>=a;i--)
+    cout<<i<<" ";
+    else 
+     for(int i=a;i>=b;i--)
     cout<<i<<" ";
     return 0;
 }

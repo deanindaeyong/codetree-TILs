c68cf0a22001a421b3548ad4ca51c2fff604e610
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 12:56:27 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/홀수만 출력/output-only-odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int a,b;
+    cin>>a>>b;
+
+    for(int i=a;a<=b;++a)
+    if(a%2!=0)
+    cout<<a<<" ";
+    return 0;
+}

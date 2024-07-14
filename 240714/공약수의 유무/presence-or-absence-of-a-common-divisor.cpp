40c7b103404790326d65ee0df7db8b1435e20bfa
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 20:15:08 +0900

    Title: 공약수의 유무, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240714/공약수의 유무/presence-or-absence-of-a-common-divisor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    cin>>a>>b;
+    bool sfd=false;
+
+    for(int i=a;i<=b;i++){
+        if(1920%i==0 ||2880%i==0){
+            sfd=true;
+        }
+    }
+    if(sfd) cout<<1;
+    else cout<<0;
+    return 0;
+}

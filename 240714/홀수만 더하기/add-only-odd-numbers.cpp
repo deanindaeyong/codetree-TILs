c87ee568b6a98122d034c11583bc712efc7fa3f8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 16:56:12 +0900

    Title: 홀수만 더하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240714/홀수만 더하기/add-only-odd-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n;
+    int sum=0;
+    for(int i=0;i<n;i++){
+        cin>>n;
+        if(n%2!=0 && n%3==0)
+        sum+=n;
+    }
+    cout<<sum;
+    return 0;
+}

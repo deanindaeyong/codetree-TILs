30a07d8eafcc44592de489b7739d52a738d43d2c
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 13:51:30 +0900

    Title: 짝수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/짝수만 출력/output-only-even.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b,i;
+    cin>>a>>b;
+    while(i=a<=b){
+        cout<<a<<" ";
+        a+=2;
+    }
+    return 0;
+}

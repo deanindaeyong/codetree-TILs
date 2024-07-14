1dd58daafbba314fb7efbc316c52ef74537d3828
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 18:25:33 +0900

    Title: 1부터 곱셈하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240714/1부터 곱셈하기/multiple-from-one.cpp
+++ 240714/1부터 곱셈하기/multiple-from-one.cpp
@@ -8,11 +8,12 @@ int main() {
     int prod=1;
     for(int i=1;i<=10;i++){
         prod*=i;
-        if(prod>n){
+        if(prod>=n){
             cout<<i;
             break;
 
         }
+      
         
  
     }

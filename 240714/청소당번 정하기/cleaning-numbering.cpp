740bb0093618364baf44d57b32c33ef91a78c76e
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 16:39:30 +0900

    Title: 청소당번 정하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240714/청소당번 정하기/cleaning-numbering.cpp
+++ 240714/청소당번 정하기/cleaning-numbering.cpp
@@ -6,14 +6,16 @@ int main() {
     int n,cnt_c=0,cnt_co=0,cnt_b=0;
     cin>>n;
     for(int i=1;i<=n;i++){
-        if(i%2==0)
-        cnt_c++;
-        if(i%3==0)
-        cnt_co++;
+        
+        
         if(i%12==0)
         cnt_b++;
+        else if(i%3==0)
+        cnt_co++;
+        else if(i%2==0)
+        cnt_c++;
 
     }
-    cout<<cnt_c-1<<" "<<cnt_co<<" "<<cnt_b;
+    cout<<cnt_c<<" "<<cnt_co<<" "<<cnt_b;
     return 0;
 }

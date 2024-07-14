50d235a4294eb283267dddf0630e0b4e279c81fa
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-14 13:55:37 +0900

    Title: n부터 1까지 숫자 출력, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240714/n부터 1까지 숫자 출력/print-n-to-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n,i;
+    cin>>n;
+    i=n; 
+    while(i>0){
+        cout<<i<<" ";
+        i--;
+    }
+    return 0;
+}

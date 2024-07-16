a3b73b0f7eaa0dd22ebd82c10771ca82ca0db4b4
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-16 06:22:49 -0700

    Title: 별표 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/별표 출력하기/print-star.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int n;
+    cin>>n;
+    int cnt=n/2-1;
+
+    for(int i=0;i<=2*n-2;i++){
+        for(int j=0;j<cnt;j++){
+            cout<<"* ";
+            
+        }
+      cout<<endl;
+      if(i>=n-1){
+        cnt--;
+      }
+      else{cnt++;}
+    }
+    return 0;
+}

e0f4904f83ef066709036c2893e2feb7afc18c4e
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-17 10:12:25 -0700

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240718/체크 다이아몬드/check-diamond.cpp
+++ 240718/체크 다이아몬드/check-diamond.cpp
@@ -7,7 +7,7 @@ int main() {
     cin>>n;
     for(int i=0;i<n;i++){
         for(int x=0;x<n-i-1;x++)cout<<" ";
-        for(int y=0;y<n+i-2;y++)cout<<"* ";
+        for(int y=1;y<(n+i);y++)cout<<"* ";
         cout<<endl;
     }
     for(int i=0;i<n-1;i++){

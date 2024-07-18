8f774e49752bf6bab490d2e9fda54a3d5c6c3971
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-18 07:25:21 -0700

    Title: 규칙을 찾아 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240718/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
+++ 240718/규칙을 찾아 출력하기/find-a-rule-and-print.cpp
@@ -7,7 +7,7 @@ int main() {
     cin>>n;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(i<=j && i!=0 && i!=n-1 )
+            if(i<=j && i!=0 && i!=n-1&& j!=n-1 )
             cout<<"  ";
             else cout<<"* ";
         }

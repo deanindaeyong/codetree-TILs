4e7bb86981cb31dd1601bc66d03aaa12e3647842
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-18 08:14:11 -0700

    Title: 모양대로 숫자 출력하기 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240719/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
+++ 240719/모양대로 숫자 출력하기 2/print-out-numbers-in-specific-shape-2.cpp
@@ -8,10 +8,11 @@ int main() {
     int cnt=2;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            if(cnt>8) cnt=2
+            if(cnt>8) cnt=2;
             cout<<cnt;
             cnt*=2;
         }
+        cout<<endl;
     }
     return 0;
 }

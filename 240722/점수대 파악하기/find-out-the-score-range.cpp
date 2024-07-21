aa6f5e23087177a5f1daa3b566c2ff1eb5bea363
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-21 09:10:35 -0700

    Title: 점수대 파악하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240722/점수대 파악하기/find-out-the-score-range.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int arr[100],count[11]={0,};
+
+    for(int i=0;i<100;i++){
+        cin>>arr[i];
+        if(arr[i]==0) break;
+        if(arr[i]<10) continue;
+        if(arr[i]==100) count[10]++;
+        else count[arr[i]/10]++;
+    }
+    for(int i=100;i>0;i-=10){
+        cout<< i<<" - "<< count[i/10]<<endl;
+    }
+    return 0;
+}

088e75ecaaa9b6d0f926a40c2adf88bfaa180fe4
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-21 08:13:54 -0700

    Title: 숫자들의 배수, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/숫자들의 배수/multiple-of-numbers.cpp
+++ 240722/숫자들의 배수/multiple-of-numbers.cpp
@@ -4,8 +4,9 @@ using namespace std;
 int main() {
     // Please write your code here.
     int n,arr[100],cnt=0;
-    cin>>arr[0]; cout<<arr[0]<<" ";
-    for(int i=1;i<100;i++){
+    cin>>arr[0];
+    for(int i=0;i<100;i++){
+        
         arr[i]=(i+1)*arr[0];
     
         cout<<arr[i]<<" ";

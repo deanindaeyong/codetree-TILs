f2d0169d7fe87705910bbca9b574172fe6be3b29
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-21 09:39:36 -0700

    Title: 나눗셈의 나머지, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240722/나눗셈의 나머지/remainder-of-division.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a,b;
+    int count[10]={0};
+    cin>>a>>b;
+    for(int i=a;i=0;i)
+    for(int i=1;i<10;i++){
+        sum+=(count[i])^2;
+    }
+    return 0;
+}

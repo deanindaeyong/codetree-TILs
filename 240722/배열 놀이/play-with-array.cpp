312b55d10623e08a22b230ee9a7985d5d8f932b7
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 01:50:59 -0700

    Title: 배열 놀이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/배열 놀이/play-with-array.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here
+    int n,q,arr[100],a,b,index=-1,s,e;
+    cin>>n>>q;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(int i=0;i<q;i++){
+        cin>>a;
+        if(a==1){
+            cin>>b;
+            cout<<arr[b-1]<<endl;;
+        }
+        if(a==2){
+            cin>>b;
+            for(int j=0;j<n;j++){
+                if(arr[j]==b){
+                    index=j+1;
+                    cout<<index<<endl;
+                    break;
+                }
+                
+            }
+            if(index==-1){
+                cout<<0<<endl;
+            }
+        }
+        if(a==3){
+            cin>>s>>e;
+            for(int k=s-1;k<e;k++){
+                cout<<arr[k]<<" ";
+            }
+        }
+    }
+    return 0;
+}

cae2a3b7837794ca605b0d2418d48ad8847ef1b6
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 07:38:12 -0700

    Title: 자동차 단일 거래 이익 최대화하기, Time: 1097ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240722/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin>>n;
+    int arr[n],cnt=0;
+    cin>>arr[0];
+    for(int i=1;i<n;i++){
+        cin>>arr[i];
+        if(arr[i-1]>arr[i]){
+            cnt++;
+        }
+    }
+    if(cnt==n-1){
+        cout<<0;
+        return 0;
+    }
+    while(true){
+        int max=0;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp_max=arr[j]-arr[i];
+                if(max<temp_max){
+                    max=temp_max;
+                }
+                
+            }
+        }
+        if(max!=0){
+            cout<<max;
+            break;
+        }
+
+    }
+    return 0;
+}

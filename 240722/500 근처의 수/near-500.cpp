f40afb34a398eb80169ae7861b782dfe67cce6eb
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 07:23:51 -0700

    Title: 500 근처의 수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240722/500 근처의 수/near-500.cpp
+++ 240722/500 근처의 수/near-500.cpp
@@ -5,7 +5,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int arr[10];
     cin>>arr[0];
-    int max=arr[0],min=1000;
+    int max=1,min=1000;
     for(int i=1;i<10;i++){
         cin>>arr[i];
         if(max<arr[i] && arr[i]<500){

dec660c4ca4f5b861216d942ac0145fc4da0960f
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 02:27:25 -0700

    Title: 연속부분수열일까, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240722/연속부분수열일까/contiguous-array-or-not.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n1,n2,cnt=0;
+    cin>>n1>>n2;
+    int arra[n1],arrb[n2];
+    for(int i=0;i<n1;i++){
+        cin>>arra[i];
+    }
+    for(int j=0;j<n2;j++){
+        cin>>arrb[j];
+    }
+    for(int i=0;i<n1;i++){
+        if(arra==arrb){
+            cnt++;
+        }
+    }
+    if(cnt==n2){
+        cout<<"Yes";
+    }
+    else cout<<"No";
+    return 0;
+}

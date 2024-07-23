37c0a4766bce93999c61a9532530d6f8e1d51f13
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-23 00:59:18 -0700

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string a[10];
+    for(int i=0;i<10;i++){
+        cin>>a[i];
+        cout<<a[i]<<endl;
+    }
+    return 0;
+}

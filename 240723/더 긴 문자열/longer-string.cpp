fa13867ef8e8a87ae42a1a14a5176f4e42820a33
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 22:49:58 -0700

    Title: 더 긴 문자열, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240723/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string x,y;
+    cin>>x>>y;
+    int xl,yl;
+    xl=x.length();yl=yl.length();
+    if(xl>yl){cout<<x<<" "<<xl;}
+    else if(yl>xl){cout<<y<<" "<<yl;}
+    else cout<<"Same";
+    return 0;
+}

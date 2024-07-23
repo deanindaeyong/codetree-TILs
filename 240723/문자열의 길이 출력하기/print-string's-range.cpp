2a9a8c6514f91c5d40597d5af05e800d61b78bf8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 22:44:17 -0700

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/문자열의 길이 출력하기/print-string's-range.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string x,y;
+    int cnt;
+    cin>>x>>y;
+    cnt=x.length()+y.length();
+    cout<<cnt;
+    return 0;
+}

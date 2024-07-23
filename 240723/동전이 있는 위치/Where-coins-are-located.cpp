fab3fed0b954346b5aae1f2768a2da5da0dda803
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-22 22:02:12 -0700

    Title: 동전이 있는 위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240723/동전이 있는 위치/Where-coins-are-located.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,m,a,b;
+    cin>>n>>m;
+    int arr[n][n]={0};
+    for(int x=0;x<m;x++){
+        cin>>a>>b;
+        arr[a-1][b-1]=1;
+
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}

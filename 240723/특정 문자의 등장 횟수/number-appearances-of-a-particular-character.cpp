2136bc59e8e39bb4ac355db619532ec09ac140c8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-23 03:48:21 -0700

    Title: 특정 문자의 등장 횟수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240723/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
+++ 240723/특정 문자의 등장 횟수/number-appearances-of-a-particular-character.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    string c;
-    cin>>c;
-    int cnt1=0;
-    int cnt2=0;
-    
-    for(int i=0;i<c.length();i+=c.find("ee")+2){
-        if(c.find("ee")!=string::npos){
-            cnt1++;
-        }
-       
-        
-    }
-    for(int j=0;j<c.length();j+=c.find("eb")+2){
-        if(c.find("eb")!=string::npos){
-            cnt2++;
-        }
-    }
-
-    cout<<cnt1<<" "<<cnt2;
+    // 문자열을 구현합니다.
+	string str;
+	
+	// 문자열을 입력받습니다.
+	cin >> str;
+	
+	// 문자열의 길이를 구합니다.
+	int len = str.length();
+	int cnt1 = 0;
+	int cnt2 = 0;
+	
+  	// 문자열 'ee'와 'eb'가 몇 번 나왔는지 확인합니다.
+  	for(int i = 0; i < len - 1; i++) {
+		if(str[i] == 'e' && str[i + 1] == 'e')
+			cnt1++;
+		if(str[i] == 'e' && str[i + 1] == 'b')
+			cnt2++;
+	}
+	
+	// 문자열 'ee'와 'eb'가 각각 몇 번 나왔는지 출력합니다.
+	cout << cnt1 << " " << cnt2;
+	
     return 0;
 }

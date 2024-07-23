50e29e1ad3650cf8a7911534634a2944d285e5a5
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-23 01:18:16 -0700

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240723/문자열에서 문자 출력/print-chars-from-word.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    string c;
+    cin>>c;
+    for(int i=0;c[i]!='\0';i++){
+        cout<<c[i]<<endl;
+    }
+    return 0;
+}

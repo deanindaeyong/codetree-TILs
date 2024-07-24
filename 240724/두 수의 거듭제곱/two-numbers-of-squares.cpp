05e91817c81863feca7387abc7e5fced5a710ded
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-24 05:10:31 -0700

    Title: 두 수의 거듭제곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/두 수의 거듭제곱/two-numbers-of-squares.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int sq(int a,int b){
+    int x=1;
+    for(int i=0;i<b;i++){
+        x*=a;
+    }
+    return x;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    cout<<sq(a,b);
+    return 0;
+}

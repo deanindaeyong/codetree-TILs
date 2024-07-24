7f7639357026d236dad31fe6ecf850dfddf2dc29
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-23 19:59:41 -0700

    Title: 반복 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/반복 출력하기/repeated-output.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void print(int n){
+    for(int i=0;i<n;i++){
+        cout<<"12345^&*()_"<<endl;
+    }
+}
+int main() {
+    // Please write your code here.
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}

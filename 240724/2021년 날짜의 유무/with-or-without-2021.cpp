8f91b16de93c0b4013a1dfae07f4b53d68e0f92b
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-24 07:03:49 -0700

    Title: 2021년 날짜의 유무, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240724/2021년 날짜의 유무/with-or-without-2021.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+bool ox(int m, int d){
+    //1,3,5,7,8,10,12
+    //4,6,9,11
+    if(m==2){
+        if(d>=1 && d<=28){
+            return true;
+        }
+        else false;
+    }else if(m<1 ||m>13||d<1||d>31){
+        return false;
+    }
+    
+    else if((m%2!=0 &&m<9 &&)||(m%2==0 &&m<13)){
+        if(d>=1 && d<=31){
+            return true;
+        }else return false;
+    }else {if(d<=30){return true;}
+    else return false;}
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int m,d;
+    cin>>m>>d;
+    if(ox(m,d)){
+        cout<<"Yes";
+    }else cout<<"No";
+    return 0;
+}

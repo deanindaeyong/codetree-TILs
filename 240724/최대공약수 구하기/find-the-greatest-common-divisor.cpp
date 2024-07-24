54bdff365e6af048b9f7f3b8ffe151ef4a76e3de
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-23 22:36:12 -0700

    Title: 최대공약수 구하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240724/최대공약수 구하기/find-the-greatest-common-divisor.cpp
+++ 240724/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -10,8 +10,8 @@ void cd(int a,int b){
         a=b;
         b=temp;
         }
-    for(int i=2;i<b;i++){
-        if(a%i==0 && b%i==0){
+    for(int i=2;i<=b;i++){
+        if(a%i==0 && b%i==0 &&i>max){
             max=i;
         }
     }

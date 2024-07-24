c450566b44e4e53bf7c70267ac4b740cf4aaa0e5
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-24 04:19:59 -0700

    Title: 함수를 이용한 소수 판별, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240724/함수를 이용한 소수 판별/decimal-decisions-using-functions.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int isprime(int a,int b){
+    int sum=0;
+    for(int i=a;i<=b;i++){
+        bool nprime=false;
+        for(int j=2;j<i;j++){
+            if(i%j==0){
+                nprime=true;
+            }
+        }
+        if(nprime){
+            continue;
+        }else sum+=i;
+        
+    }
+    return sum;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    cout<<isprime(a,b);
+    return 0;
+}

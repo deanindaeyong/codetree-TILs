e07d8083bbeda0cc32a8f7e2d3ceba966eb0842e
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-24 09:46:22 -0700

    Title: 두 정수에 대한 연산값, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/두 정수에 대한 연산값/operational-values-for-two-integers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+void pls(int &a, int &b){
+    if(a>b){
+        a+=25;
+        b*=2;
+    }else {a*=2;b+=25;}
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a,b;
+    cin>>a>>b;
+    pls(a,b);
+    cout<<a<<" "<<b;
+
+    return 0;
+}

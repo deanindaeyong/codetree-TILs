b49cbfe0e476056c9566bdf0716ce51b7cea2ff9
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-25 03:14:06 -0700

    Title: 재귀함수를 이용한 별 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240725/재귀함수를 이용한 별 출력/star-output-with-recursive-function.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int n;
+void star(int a){
+    if(a==0){
+        return;
+    }
+    star(a-1);
+    for(int i=0;i<a;i++){
+        cout<<"*";
+    }cout<<endl;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    star(n);
+    return 0;
+}

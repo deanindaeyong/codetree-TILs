b2cf81bf59f1e172f0495f2e27d46dded10a7419
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-25 06:14:18 -0700

    Title: 홀수 짝수에 따른 출력값, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240725/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int n;
+int dep(int a){
+    if(a==1){
+        return 1;
+    }
+    else if(a==0)
+        return 0;
+    if(a%2==0)
+        return dep(a/2)+a;
+    if(a%2!=0)
+        return dep(a-2)+a;
+    
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    cout<<dep(n);
+
+    return 0;
+}

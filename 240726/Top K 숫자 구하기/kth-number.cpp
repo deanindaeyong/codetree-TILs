718a81cf55e85499f9352900c95cc51bbfcad66c
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-25 19:11:51 -0700

    Title: Top K 숫자 구하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240726/Top K 숫자 구하기/kth-number.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n,k,arr[1000];
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>k;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sort(arr,arr+n);
+    cout<<arr[k-1];
+    return 0;
+}

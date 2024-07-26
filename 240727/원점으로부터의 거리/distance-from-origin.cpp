9656e1b08bc56b22944d417269b377ed058ed230
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-26 08:14:50 -0700

    Title: 원점으로부터의 거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240727/원점으로부터의 거리/distance-from-origin.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n;
+class Dis{
+    public:
+    int x,y,r;
+    Dis(int x,int y,int r):x(x),y(y),r(r){}
+    Dis(){}
+};
+bool comp(const Dis &a,const Dis &b){
+    
+    return ((a.x)*(a.x)+(a.y)*(a.y))<((b.x)*(b.x)+(b.y)*(b.y));
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    int a,b;
+    Dis dis[n];
+    
+    for(int i=0;i<n;i++){
+        cin>>a>>b;
+        dis[i]=Dis(a,b,i+1);
+
+    }
+    sort(dis,dis+n,comp);
+  
+    for(int i=0;i<n;i++){
+        cout<<dis[i].r<<endl;
+    }
+    return 0;
+}

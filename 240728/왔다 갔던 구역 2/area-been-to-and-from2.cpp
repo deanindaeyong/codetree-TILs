509c1ea155b279ab3c0ee87ac82f36e69f0d3377
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-27 09:02:38 -0700

    Title: 왔다 갔던 구역 2, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240728/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n,x,arr[30]={0,};
+    char y;
+    cin>>n;
+    
+    for(int i=0;i<n;i++){
+        int curr=0,x1=0;
+        cin>>x>>y;
+        x1=x;
+        x+=15;
+        if(i==0){
+            curr=0;
+        }else curr=x;
+        
+        if(y=='R'){
+
+        for(int j=curr;j<curr+x1;j++){
+            arr[j]++;
+            }
+        }
+        else{
+            for(int j=curr;j>curr-x1;j--){
+            arr[j]++;
+            }
+
+        }
+    }
+    int cnt=0;
+    for(int i=0;i<30;i++){
+        if(arr[i]>=2){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+    return 0;
+}

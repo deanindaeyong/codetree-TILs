648c5db2f539ad1e66d67101ca68cc405d0fcc98
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-26 20:51:40 -0700

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240727/요일 맞추기/guess-day-of-week.cpp
+++ 240727/요일 맞추기/guess-day-of-week.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 string day(int m1,int d1,int m2, int d2){
     int sum1=0,sum2=0;
-    string ds[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun",};
+    string ds[7]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
     int mon[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
     for(int i=0;i<m1;i++){
         sum1+=mon[i];
@@ -13,11 +13,13 @@ string day(int m1,int d1,int m2, int d2){
     }
     sum1+=d1;
     sum2+=d2;
-    
-    int total=sum2-sum1;
+    int total=0;
+    if(sum2>sum1){
+        total=sum2-sum1;
+    }else total=sum1-sum2;
     int dat=1;
     
-    dat=abs(total)%7+2;
+    dat=(total+3)%7;
     return ds[dat];
 }
 int main() {

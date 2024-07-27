df48b9faa085ebfbf95dc5b834951ede716a0a93
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-26 21:22:24 -0700

    Title: 요일 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240727/요일 맞추기/guess-day-of-week.cpp
+++ 240727/요일 맞추기/guess-day-of-week.cpp
@@ -3,29 +3,32 @@
 using namespace std;
 string day(int m1,int d1,int m2, int d2){
     int sum1=0,sum2=0;
-    string ds[7]={"Sun","Mon","Tue","Wed","Thu","Fri","Sat"};
+    string ds[7]={"Mon","Tue","Wed","Thu","Fri","Sat","Sun"};
     int mon[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
     for(int i=0;i<m1;i++){
         sum1+=mon[i];
     }
+ 
     for(int j=0;j<m2;j++){
         sum2+=mon[j];
     }
     sum1+=d1;
     sum2+=d2;
     int total=0;
-    if(sum2>sum1){
-        total=sum2-sum1;
-    }else total=sum1-sum2;
-    int dat=1;
-    
-    dat=(total+3)%7;
-    return ds[dat];
+
+    total=sum2-sum1;
+    if(total<0){
+        while(total<7){
+            total+=7;}
+    }
+    total%=7;
+
+    return ds[total];
 }
 int main() {
     // 여기에 코드를 작성해주세요.
     int m1,m2,d1,d2;
-    cin>>m1>>m2>>d1>>d2;
+    cin>>m1>>d1>>m2>>d2;
     cout<<day(m1,d1,m2,d2);
     return 0;
 }

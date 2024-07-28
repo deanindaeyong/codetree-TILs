dfeeb54b18b6d5ab110f2b02d1373f94f9539bf6
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-28 09:31:09 -0700

    Title: 벌금은 누구에게, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240729/벌금은 누구에게/who-will-pay.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int n,m,k;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>m>>k;
+    int arr[n]={0,},t;
+    int x=-1;
+    for(int i=0;i<m;i++){
+        cin>>t;
+        arr[t]+=1;
+        for(int j=1;j<=n;j++){
+            if(arr[j]>=k){
+                x=j;
+                break;
+            }
+        }
+    }
+    cout<<x;
+    return 0;
+}

72aaaafec4e166af28fabc2587ac6f17153e5f7c
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-29 01:15:12 -0700

    Title: 문자에 따른 명령 2, Time: 15ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240729/문자에 따른 명령 2/text-based-commands2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+string d;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
+    int curr_x=0,curr_y=0;
+    int dir=0;
+    cin>>d;
+    for(int i=0;i<d.length();i++){
+        if(d[i]=='L'){
+            dir=(dir-1+4)%4;
+       //     curr_x+=dx[dir];curr_y+=dy[dir];
+         //   cout<<curr_x<<" "<<curr_y<<endl;
+        }else if(d[i]=='R'){
+            dir=(dir+1)%4;
+          //  curr_x+=dx[dir];curr_y+=dy[dir];
+        }else{
+            dir=dir;
+            curr_x+=dx[dir];curr_y+=dy[dir];
+         //   cout<<curr_x<<" "<<curr_y<<endl;
+            } 
+    }
+    cout<<curr_x<<" "<<curr_y;
+    return 0;
+}

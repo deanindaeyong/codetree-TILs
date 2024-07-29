2450308941cc288634f4928307d865f6b12b9a15
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-29 06:31:23 -0700

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240729/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
+++ 240729/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -3,18 +3,12 @@ using namespace std;
 int n,m;
 
 int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
-
+int arr[100][100]={0,};
 
 bool Inrange(int a,int b){
     return(0<=a && a<n && 0<=b && b<m );
 }
-
-
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int arr[100][100]={0,};
-    cin>>n>>m;
-    arr[0][0]=1;
+void Simulate(){
     int t=n*m;
     int x=0,y=0,dir=0;
     
@@ -23,18 +17,28 @@ int main() {
         if(!Inrange(nx,ny) || arr[nx][ny]!=0){
             dir=(dir+1)%4;
         }
-        x=x+dx[dir];
-        y=y+dy[dir];
+        x=x+dx[dir];y=y+dy[dir];
         arr[x][y]=i;
+ 
     
     }
+}
+void Printsquare(){
     for(int i=0;i<n;i++){
         for(int j=0;j<m;j++){
             cout<<arr[i][j]<<" ";
         }
         cout<<endl;
     }
-   
-   
+
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    cin>>n>>m;
+    arr[0][0]=1;
+    Simulate();
+    Printsquare();
+    
     return 0;
 }

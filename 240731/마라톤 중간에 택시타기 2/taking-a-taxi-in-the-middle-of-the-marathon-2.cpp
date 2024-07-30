079758ffb42ac6daf03e099f957f82b0637a9d01
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-30 08:21:05 -0700

    Title: 마라톤 중간에 택시타기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240731/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
+++ 240731/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.cpp
@@ -1,29 +1,39 @@
 #include <iostream>
 #include <climits>
+#include <algorithm>
+#include <cstdlib>
+
+#define MAX_N 100
 
 using namespace std;
+
 int n;
-int dis(int a,int b, int a1,int b1){
-    return abs(a-a1)+abs(b-b1);
-}
+int x[MAX_N], y[MAX_N];
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-
-
-    cin>>n;
-    int x[100],y[100];
-    for(int i=0;i<n;i++){
-        cin>>x[i]>>y[i];
-    }
-    int min=INT_MAX;
-    for(int i=1;i<n-1;i++){
-        for(int j=1;j<n-1;j++){
-            if(dis(x[0],y[0],x[i],y[i])+dis(x[i],y[i],x[n-1],y[n-1])<min){
-                min=dis(x[0],y[0],x[i],y[i])+dis(x[i],y[i],x[n-1],y[n-1]);
-               // cout<<" i= "<<i<<" j= "<<j<<endl;
-            }
-        }
-    }
-    cout<<min;
+    // 변수 선언 및 입력
+    cin >> n;
+	for(int i = 0; i < n; i++)
+		cin >> x[i] >> y[i];
+	
+	// 각 i번째 체크포인트를 건너 뛰었을 때의 거리를 구해줍니다.
+	int ans = INT_MAX;
+    for(int i = 1; i < n - 1; i++) {
+		// 거리를 구합니다.
+		int dist = 0;
+		int prev_idx = 0;
+    	for(int j = 1; j < n; j++) {
+			if(j == i) continue;
+        	dist += abs(x[prev_idx] - x[j]) + abs(y[prev_idx] - y[j]);
+			prev_idx = j;
+		}
+		
+		// 가능한 거리 중 최솟값을 구해줍니다.
+		ans = min(ans, dist);
+	}
+    
+    // 출력
+    cout << ans;
+	
     return 0;
 }

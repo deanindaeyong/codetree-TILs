191793073d00420d74e249de4ddc8216e8d44994
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-07-31 22:50:52 +0900

    Title: 특정 수와 근접한 합, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240731/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+int s,n;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>s;
+    int arr[100];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    //  6 20
+//  5 7 9 1 13 8
+    int min=INT_MAX;
+    min=min(min,s-sum);
+    for(int i=0;i<n;i++){
+        for(int j=i+1;j<n;j++){
+            int sum=0;
+            for(int k=0;k<n;k++){
+                
+                if(k==i||k=j)continue;
+                else{
+                    sum+=arr[k];
+                }
+
+            }
+            min=min(min,s-sum);
+        }
+    }
+cout<<sum;
+    return 0;
+}

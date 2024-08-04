77fe90eeb4843f3e79a34261b54fe40a66b5dffb
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-04 18:39:13 +0900

    Title: 겹치지 않는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240804/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int n;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int ans=INT_MIN;
+    cin>>n;
+    int x1[100],x2[100];
+    for(int i=0;i<n;i++){
+        cin>>x1[i]>>x2[i];
+    }
+    int cnt=n;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) continue;
+            if((x1[i]>x1[j]&&x2[i]<x2[j])||(x1[i]<x1[j]&&x2[i]>x2[j])){
+                cnt--;
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}

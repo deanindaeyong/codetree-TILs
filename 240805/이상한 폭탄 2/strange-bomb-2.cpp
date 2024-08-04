e33a209adcd2d78b838a6add802794d0b19d9c31
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-05 00:38:19 +0900

    Title: 이상한 폭탄 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240805/이상한 폭탄 2/strange-bomb-2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int n,k;
+int main() {
+    cin>>n>>k;
+    int x[100];
+    for(int i=0;i<n;i++){
+        cin>>x[i];
+    }
+    int ans=INT_MIN;
+    int index;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            if(i==j) continue;
+            if(x[i]==x[j]&& abs(i-j)<=k){
+                index=x[i];
+            }
+            ans=max(ans,index);
+        }
+    }
+    cout<<ans;
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}

07ff754b268d7996ba43d9d639d6c5f2a7d4e377
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-06 01:54:25 +0900

    Title: 흥미로운 숫자 2, Time: 62ms, Memory: 0MB, Status: Passed - CodeTree

--- 240806/흥미로운 숫자 2/interesting-numbers-2.cpp
+++ 240806/흥미로운 숫자 2/interesting-numbers-2.cpp
@@ -1,33 +1,48 @@
 #include <iostream>
+#include <string>
 using namespace std;
-bool Int_number(string a){
-    int len=a.length();
-    int cnt=0;
-    for(int i=0;i<len;i++){
-        for(int j=0;j<len;j++){
-            if(i==j)continue;
-            if(a[i]==a[j]){
-                cnt++;
-                break;
+
+// 흥미로운 숫자인지 확인하는 함수
+bool isInteresting(int num) {
+    string str = to_string(num);
+    int len = str.length();
+    
+    for (int i = 0; i < len; ++i) {
+        // 각 자리를 기준으로 다른 숫자 하나로 교체
+        char original = str[i];
+        for (char c = '0'; c <= '9'; ++c) {
+            if (c == original) continue; // 같은 숫자로 교체하는 것은 무의미
+            
+            str[i] = c;
+            // 나머지 자리가 모두 같은 숫자인지 확인
+            bool is_valid = true;
+            char first = str[0];
+            for (int j = 1; j < len; ++j) {
+                if (str[j] != first) {
+                    is_valid = false;
+                    break;
+                }
             }
+            if (is_valid) return true; // 흥미로운 숫자 발견
         }
-    } 
-    if(len-1==cnt){
-        return true;
+        // 원래 숫자로 복원
+        str[i] = original;
     }
-   else return false;
+    return false;
 }
-int x,y;
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin>>x>>y;
-    int cntt=0;
-    for(int i=x;i<=y;i++){
-        string m=to_string(i);
-        if(Int_number(m)){
-            cntt++; 
+    int X, Y;
+    cin >> X >> Y;
+
+    int count = 0;
+    for (int i = X; i <= Y; ++i) {
+        if (isInteresting(i)) {
+            ++count;
         }
     }
-    cout<<cntt;
+
+    cout << count << endl;
+
     return 0;
 }

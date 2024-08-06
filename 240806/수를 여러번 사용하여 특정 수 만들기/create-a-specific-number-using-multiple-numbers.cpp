fcf0e4a5c95225298b2d59de094d2ce8744e42a8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-06 23:46:31 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 1065ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 240806/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+int a,b,c;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>a>>b>>c;
+    int maxx=INT_MIN;
+    for(int i=c;i>0;i--){
+        for(int j=0;j<=1000;j++){
+            for(int k=0;k<=1000;k++){
+                if(j*a+b*k<=i){
+                    maxx=max(maxx,j*a+b*k);
+                    
+                }
+            }
+        }
+    }
+    cout<<maxx;
+    return 0;
+}

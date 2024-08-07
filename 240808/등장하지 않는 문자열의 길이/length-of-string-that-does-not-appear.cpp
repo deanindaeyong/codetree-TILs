754ef2b42a7fb849394c57474a4a6cf363fd65b3
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-08 01:28:05 +0900

    Title: 등장하지 않는 문자열의 길이, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240808/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
+++ 240808/등장하지 않는 문자열의 길이/length-of-string-that-does-not-appear.cpp
@@ -15,12 +15,13 @@ int main() {
         
         if(!find){
             temp=a.substr(i,cnt+1); // 
-        }else {temp+=a.substr(i+1,1);} //1 
+        }else {temp+=a.substr(i,1);cout<<temp<<endl;} //1 
         for(int j=i+1;j<n;j++){     
             if(temp==a.substr(j,cnt+1)){// 1 2
                 cnt++;
                 find=true;
-               
+                cout<<"i= "<<i <<" j= "<<j<<endl;
+             //   cout<<temp<<endl;
                 break;
             }else find =false;
         }

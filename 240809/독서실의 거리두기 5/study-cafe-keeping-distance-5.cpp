b3f94229fbb6032206578a78198acf7cee83fca8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-09 23:11:08 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240809/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 240809/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -8,31 +8,31 @@ int main() {
     cin>>n;
     cin>>a;
     
-    int maxx=INT_MIN,prev_index=0,after_index=0,cnt=0;
+    int maxx=INT_MIN,prev_index=0,after_index=0,cnt=0,minn=INT_MAX;
     for(int i=0;i<n;i++){
-        if(a[i]!='1') continue;
-        
-        for(int j=i+1;j<n;j++){
-            if(i==j) continue;
-            if(a[j]!='1') continue;
-            
-           for(int k=i+1;k<j;k++){
-                if(a[k]=='0'){
-                    cnt++;
-                }
-           }
-           if(cnt>maxx){
-            maxx=cnt;
-            after_index=j;
-            prev_index=i;
-           }
-        }
+        if(a[i]=='1') continue;
+        a[i]='1';
 
-        
+        for(int j=0;j<n;j++){
+            if(i==0||i==n-1){
+                prev_index=j;
+                after_index=j;
+            }
+            if(j<i &&a[j]=='1'){
+                prev_index=j;
+                continue;
+            }
+            if(j>i&&a[j]=='1'){after_index=j;break;}
+            if(i==j) continue;
+
+           
+        }
+        minn=min(abs(prev_index-i),abs(after_index-i));
+        maxx=max(maxx,minn);
+        a[i]='0';
     }
    
+cout<<maxx;
 
-maxx=abs(prev_index-after_index)/2;
-    cout<<maxx;
     return 0;
 }

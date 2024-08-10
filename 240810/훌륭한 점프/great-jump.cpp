caf3d089000ae14dc3baf0ae1a14dfb9e69d4f32
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-10 21:50:09 +0900

    Title: 훌륭한 점프, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240810/훌륭한 점프/great-jump.cpp
+++ 240810/훌륭한 점프/great-jump.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 using namespace std;
-int n,k;
-int a[100];
-//a는 최댓값
-bool ok(int max_val){
-    int available_indices[100]={};
-    int cnt=0;
-    for(int i=0;i<n;i++ ){
-        if(a[i]<=max_val){
-            available_indices[cnt++]=i;
-        }
-    }
-    for(int i=1;i<cnt;i++){
-        int dis=available_indices[i]-available_indices[i-1];
-        if(dis>k)
-        return false;
-    }
-    return true;
-}
-int main() {
-    // 여기에 코드를 작성해주세요.
-    cin>>n>>k;
-    int maxxx=0;
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-        maxxx=max(maxxx,a[i]);
-    }
-    int minn=100,maxx=-1;
-    for(int i=1;i<=max(a[0],a[n-1]);i++){
-        if(ok(i)){
-            maxx=max(maxx,i);
 
+int n, k;
+int a[100];
+
+bool ok(int max_val) {
+    int last_position = 0; // 1번 돌에서 시작 (0번째 인덱스)
+    for (int i = 1; i < n; i++) { // 2번 돌부터 n번 돌까지 확인
+        if (a[i] <= max_val) {
+            if (i - last_position <= k) {
+                last_position = i;
+            }
         }
-        
     }
-//cout<<minn<<endl;
-    cout<<maxx;
+    return last_position == n - 1; // n번 돌에 도달했는지 확인
+}
+
+int main() {
+    cin >> n >> k;
+    int maxxx = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        maxxx = max(maxxx, a[i]);
+    }
+
+    int minn = 100; // 최댓값의 최소를 구하기 위한 초기값 설정
+    for (int i = 1; i <= maxxx; i++) { // maxxx부터 1까지 내려오면서 확인
+        if (ok(i)) {
+            minn = i;
+            break; // 최솟값을 찾았으므로 더 이상 탐색할 필요 없음
+        }
+    }
+
+    cout << minn;
     return 0;
 }

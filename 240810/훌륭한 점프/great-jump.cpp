e25115b224e85f6c61f8e61aad3f835177b4cf7c
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-10 18:56:10 +0900

    Title: 훌륭한 점프, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240810/훌륭한 점프/great-jump.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+int n,k;
+int a[100];
+//a는 최댓값
+bool ok(int max_val){
+    int available_indices[100]={};
+    int cnt=0;
+    for(int i=0;i<n;i++ ){
+        if(a[i]<=max_val){
+            available_indices[cnt++]=i;
+        }
+    }
+    for(int i=1;i<cnt;i++){
+        int dis=available_indices[i]-available_indices[i-1];
+        if(dis>k)
+        return false;
+    }
+    return true;
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n>>k;
+    int maxxx=0;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+        maxxx=max(maxxx,a[i]);
+    }
+    int minn=100,maxx=-1;
+    for(int i=2;i<=maxxx;i++){
+        if(ok(i)){
+            maxx=max(maxx,i);
+            cout<<maxx<<endl;
+            break;
+        }
+        
+    }
+
+    //cout<<maxx;
+    return 0;
+}

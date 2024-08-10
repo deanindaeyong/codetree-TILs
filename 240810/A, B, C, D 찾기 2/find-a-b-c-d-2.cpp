8eff98e337095ce8f5b8a0f8fe932b05db0bf0ef
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-10 23:40:29 +0900

    Title: A, B, C, D 찾기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240810/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
+++ 240810/A, B, C, D 찾기 2/find-a-b-c-d-2.cpp
@@ -4,11 +4,6 @@ using namespace std;
   int a,b,c,d;
   int arr[15];
 
-/*
-a,b,c,d
-a+b, b+c, c+d, d+a,a+c,b+d,
-a+b+c, a+b+d,a+c+d, b+c+d
-a+b+c+d*/
 int main() {
     // 여기에 코드를 작성해주세요.
   
@@ -17,12 +12,25 @@ int main() {
         cin>>arr[i];
     }
     sort(arr,arr+15);
-    a=arr[0];b=arr[1];
+    a=arr[0];
+    b=arr[1];
     if(a+b!=arr[2]){
         c=arr[2];
     }else c=arr[3];
-    d=arr[4];
-               
-     cout<<a<<" "<<b<<" "<<c<<" "<<d;
+    d=arr[14]-a-b-c;
+    cout<<a<<" "<<b<<" "<<c<<" "<<d;
     return 0;
 }
+/*
+    for(int i=1;i<=40;i++){
+        for(int j=1;j<=40;j++){
+            for(int k=1;k<=40;k++){
+                for(int l=1;l<=40;l++){
+                    if(cor(i,j,k,l)){
+                        cout<<i<<" "<<j<<" "<<k<<" "<<l;
+                        return 0;
+                    }
+                }
+            }
+        }
+    }*/

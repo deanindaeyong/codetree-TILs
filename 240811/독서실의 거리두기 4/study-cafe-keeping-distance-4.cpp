0936c7220d0129a4c4cd6d0eb10e9149cf54c46a
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-11 00:42:26 +0900

    Title: 독서실의 거리두기 4, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240811/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
+++ 240811/독서실의 거리두기 4/study-cafe-keeping-distance-4.cpp
@@ -10,26 +10,26 @@ int main() {
     cin>>a;
     int max_dist=0;
     for(int i=0;i<n;i++){
-        if(a[i]=='1') continue;
+        if(a[i]=='1')continue;
         a[i]='1';
-        for(int j=i+1;j<n;j++){
-            if(a[j]=='1')continue;
+        for(int j=i;j<n;j++){
+            if(a[j]=='1') continue;
             a[j]='1';
-            int min_dist = INT_MAX;
-            int prev_index = -1;
-            
+            int prev_index=-1;
+            int min_dis=INT_MAX;
             for(int k=0;k<n;k++){
                 if(a[k]=='1'){
                     if(prev_index!=-1){
-                        int dist=k-prev_index;
-                        min_dist=min(min_dist,dist);
+                        int dis=k-prev_index;
+                        min_dis=min(dis,min_dis);
+
                     }
                     prev_index=k;
                 }
             }
-            max_dist = max(max_dist, min_dist);
-            a[j]='0';
+            max_dist=max(max_dist,min_dis);
 
+            a[j]='0';
         }
         a[i]='0';
     }

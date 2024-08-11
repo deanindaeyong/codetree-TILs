6ad2eacc17e8d8d0183b1d14c751ea3b7e06e414
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-11 22:44:15 +0900

    Title: 이상한 폭탄 3, Time: 11ms, Memory: 7MB, Status: Passed - CodeTree

--- 240811/이상한 폭탄 3/strange-bomb-3.cpp
+++ 240811/이상한 폭탄 3/strange-bomb-3.cpp
@@ -4,17 +4,19 @@ int n,k;
 int main() {
     // 여기에 코드를 작성해주세요.
     cin>>n>>k;
-    int a[1000001],ans[1000001]={0,};
+    int a[1000001]={0,},ans[1000001]={0,};
     for(int i=0;i<n;i++){
         cin>>a[i];
     }
+    bool found =false;
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             if(i==j) continue;
             if(a[i]==a[j]){
                 if(abs(i-j)<=k){
                     ans[a[i]]++;
-                    //cout<<i<<" j= "<<j<<" ans[1]= "<<ans[1]<<endl;
+                    found=true;
+                  //  cout<<i<<" j= "<<j<<" ans[1]= "<<ans[1]<<endl;
                 }
             }
 
@@ -25,10 +27,12 @@ int main() {
         if(maxx<=ans[i]){
             index=i;
             maxx=ans[i];
+            //cout<<index;
         }
     }
+    if(!found){
+        cout<<0;
+    }else cout<<index;
     //cout<<ans[0]<<" "<<ans[1]<<" "<<ans[2];
-    cout<<index;
-
     return 0;
 }

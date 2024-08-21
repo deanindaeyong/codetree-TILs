e1908c5dd467d958edca9a030b9cbd86a99cf538
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-21 23:07:58 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240821/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240821/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -1,21 +1,54 @@
 #include <iostream>
 using namespace std;
 int a,b,c;
+
 int main() {
-    // 여기에 코드를 작성해주세요.
     
     cin>>a>>b>>c;
     int cnt=0;
-    // cnt ==0
-    if(c-b==1 && b-a==1){
-        cnt=0;
-    }
-    //b-a=1
-    else if(b-a<=2 || c-b<=2){
+    //무조건 양쪽 으로만 움직이고
+
+    while(c-b!=1 ||b-a!=1){ // 세자리 같음
+        // 2개 연속 
+        if(c-b==1){
+            // 4 6 7
+           if(b-a==2) {
+            c=a+1;
+            swap(c,b);
+            //3 6 7
+           }else {
+            c=a+2;
+            swap(c,b);}
+        }
+        // 4 5
+        else if(b-a==1){
+            // 7
+            if(c-b==2){
+                a=c-1;
+                swap(a,b);
+            }else {
+                a=c-2;
+                swap(b,a);
+            }
+
+        }else {
+            if(c-b>b-a){
+                a=c-2;
+                swap(a,b);
+            }
+            else if(b-a>c-b){
+                c=a+2;
+                swap(b,c);
+                
+            }else{
+                c=a+1;
+                swap(b,c);
+            }
+
+        }
+   
         cnt++;
-        
-    }else cnt=2;
-    
+    }
     cout<<cnt;
     return 0;
 }

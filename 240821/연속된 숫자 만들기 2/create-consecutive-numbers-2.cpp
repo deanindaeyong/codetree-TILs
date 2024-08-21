e71cc55624efc90f8acb7e69a471b9de0ea981d8
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-08-21 21:35:53 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240821/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 240821/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -11,38 +11,11 @@ int main() {
         cout<<cnt;
     }
     //b-a=1
-    else if(b-a==1 || c-b==1){
+    else if(b-a<=2 || c-b<=2){
         cnt++;
         
     }else cnt=2;
-
-
-
-
-    /*while(c-b!=1 || b-a!=1){
-        int temp1;
-        //a b c
-        //b a c
-        if(b-a>c-b){
-            
-            a=b+1;
-            temp1=a;
-            a=b;
-            b=temp1;
-            
-        //a b c
-        //a c b
-        }else{
-            c=(a+b)/2;
-            temp1=c;
-            c=b;
-            b=temp1;
-        }
-        cout<<a<<" "<<b<<" "<<c<<endl;
-        cnt++;
-
-    }
-    */
+    
     cout<<cnt;
     return 0;
 }

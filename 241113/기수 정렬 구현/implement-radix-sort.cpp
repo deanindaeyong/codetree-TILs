0008f6e9d667cbffecefd4c91114f51ede22e370
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-11-13 02:11:11 -0800

    Title: 기수 정렬 구현, Time: 12ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 241113/기수 정렬 구현/implement-radix-sort.cpp
+++ 241113/기수 정렬 구현/implement-radix-sort.cpp
@@ -1,4 +1,41 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+#define MAX_N 100000
+#define MAX_K 6
+#define MAX_DIGIT 10
+int n,arr[MAX_N];
+void radix_sor(){
+    int p=1;
+    vector<int> new_arr[MAX_DIGIT];
+    for(int pos=0;pos<MAX_K;pos++){
+        for(int i=0;i<n;i++){
+            int digit=(arr[i]/p)%10;
+            new_arr[digit].push_back(arr[i]);
+        }
+        int index=0;
+        for(int i=0;i<MAX_DIGIT;i++){
+            for(int j=0;j<(int)new_arr[i].size();j++){
+                arr[index++]=new_arr[i][j];
+            }
+            
+        }
+        p*=10;
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    radix_sor();
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
+/*#include <iostream>
 using namespace std;
 int n,arr[100000];
 int set_digit(int num,int pos){
@@ -38,4 +75,4 @@ int main() {
         cout<<arr[i]<<" ";
     }
     return 0;
-}
+}*/

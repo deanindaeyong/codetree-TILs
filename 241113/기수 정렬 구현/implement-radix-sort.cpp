7b3bd5a20909a8a57fa1f91143b034e94bd887fc
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-11-13 01:50:30 -0800

    Title: 기수 정렬 구현, Time: 16ms, Memory: 4MB, Status: Wrong Answer - CodeTree

---
+++ 241113/기수 정렬 구현/implement-radix-sort.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+int n,arr[100000];
+int set_digit(int num,int pos){
+    while(pos>0){
+        num=num/10;
+        pos--;
+    }
+    num=num%10;
+    return num;
+}
+
+void radix_sort(){
+    for(int pos=6;pos>=0;pos--){
+        int arr_new[10][100000]={};
+        int bucket[10]={};
+        for(int i=0;i<n;i++){
+            int digit=set_digit(arr[i],pos);
+            arr_new[digit][bucket[digit]++]=arr[i];
+        }
+        int index=0;
+        for(int i=0;i<10;i++){
+            for(int j=0;j<bucket[i];j++){
+                arr[index++]=arr_new[i][j];
+            }
+        }
+      
+    }
+}
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin>>n;
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    radix_sort();
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}

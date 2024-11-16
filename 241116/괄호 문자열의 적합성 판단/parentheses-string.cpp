f4878d8ed61e2e75e01938ae6fa3d1381d9ef226
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2024-11-16 01:28:36 -0800

    Title: 괄호 문자열의 적합성 판단, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241116/괄호 문자열의 적합성 판단/parentheses-string.cpp
+++ 241116/괄호 문자열의 적합성 판단/parentheses-string.cpp
@@ -6,7 +6,7 @@ int main() {
     // 여기에 코드를 작성해주세요.
     stack<char> st;
     string x;
-    bool correct=false;
+    bool correct=true;
     cin>>x;
     for(int i=0;i<x.length();i++){
         if(x[i]=='('){

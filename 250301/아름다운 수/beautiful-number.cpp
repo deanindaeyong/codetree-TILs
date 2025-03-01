1117f798e1be82a25d4d8abfea8b7f05a5a3c202
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-02-28 18:44:56 -0800

    Title: 아름다운 수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 250301/아름다운 수/beautiful-number.cpp
+++ 250301/아름다운 수/beautiful-number.cpp
@@ -2,43 +2,38 @@
 #include <vector>
 using namespace std;
 
-int n, btcnt = 0;
+int n,btcnt=0;
 vector<int> ans;
-
-bool check(vector<int>& x) {
-    int i = 0;
-    while (i < n) {
-        int cur = x[i];
-        int cnt = 0;
-        if(cur==1) {i++;continue;}
-        while (i < n && x[i] == cur) {
+bool check(vector<int>& x){
+    int i=0;
+    while(i<n){
+        int cur=x[i];
+        int cnt=0;
+        while(i<n&&cur==x[i]&&cnt<cur){
             cnt++;
             i++;
         }
-        if (cur != cnt) {
-            return false;
-        }
+        if(cur!=cnt) return false;
     }
     return true;
 }
-
-void func(int cur_num) {
-    if (cur_num == n + 1) {
-        if (check(ans)) {
+void fuc(int cur_num){
+    if(cur_num==n+1){
+        if(check(ans)){
             btcnt++;
         }
         return;
     }
-    for (int i = 1; i <= 9; i++) {
+    for(int i=1;i<=4;i++){
         ans.push_back(i);
-        func(cur_num + 1);
+        fuc(cur_num+1);
         ans.pop_back();
     }
 }
-
 int main() {
-    cin >> n;
-    func(1);
-    cout << btcnt;
+    cin>>n;
+    fuc(1);
+    cout<<btcnt;
+    // Please write your code here.
     return 0;
 }

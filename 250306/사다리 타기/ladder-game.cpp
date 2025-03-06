99a60e64e9a53266612fe3dcc3e87ad21a0740a0
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-03-06 03:26:46 -0800

    Title: 사다리 타기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 250306/사다리 타기/ladder-game.cpp
+++ 250306/사다리 타기/ladder-game.cpp
@@ -1,45 +1,46 @@
 #include <iostream>
-#include <climits>
 #include <vector>
+#include <climits>
 #include <algorithm>
+
 using namespace std;
 int n,m;
-vector<pair<int,int>> ladder;
-
-vector<int> finalpos;
 int minn=INT_MAX;
-vector<int> play_game(const vector<pair<int,int>>& ladders){
-    vector<int> position(n+1);
-    for(int i=1;i<=n;i++) position[i]=i;
-    for(const auto& l:ladders){
-        swap(position[l.second],position[l.second+1]);
+vector<int> final_pos;
+vector<pair<int,int>> ladders;
+vector<int> play_game(const vector<pair<int,int>> ld){
+    vector<int> position(n);
+    for(int i=0;i<n;i++){
+        position[i]=i;
+    }
+    for(const auto& l:ld){
+        swap(position[l.second-1],position[l.second]);
     }
     return position;
 }
-void min_dis(int idx,vector<pair<int,int>> selected_ladder){
-    if(play_game(selected_ladder)==finalpos){
-   
-        minn=min((int)selected_ladder.size(),minn);
+void func(int idx,vector<pair<int,int>> selected_ladder){
+    if(play_game(selected_ladder)==final_pos){
+        minn=min(minn,(int)selected_ladder.size());
         return;
     }
     for(int i=idx;i<m;i++){
-        selected_ladder.push_back(ladder[i]);
-        min_dis(i+1,selected_ladder);
+        selected_ladder.push_back(ladders[i]);
+        func(i+1,selected_ladder);
         selected_ladder.pop_back();
     }
-    
-
 }
+
 int main() {
     cin>>n>>m;
-    ladder.resize(m);
+    ladders.resize(m);
     for(int i=0;i<m;i++){
-        cin>>ladder[i].second>>ladder[i].first;
+        cin>>ladders[i].second>>ladders[i].first;
     }
-    sort(ladder.begin(),ladder.end());
-    finalpos=play_game(ladder);
-    min_dis(0,{});
-    cout<<minn;
+    
+    sort(ladders.begin(),ladders.end());
+    final_pos=play_game(ladders);
+    func(0,{});
     // Please write your code here.
+    cout<<minn;
     return 0;
 }

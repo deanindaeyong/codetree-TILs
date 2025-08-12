efa6c6e7dbbc31cd5cbb77e9fb7752bab4420345
Deanindaeyong <72272134+deanindaeyong@users.noreply.github.com>
2025-08-13 00:37:14 +0900

    Title: 알파벳과 사칙연산, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 250812/알파벳과 사칙연산/calculations-with-alphabet.cpp
+++ 250812/알파벳과 사칙연산/calculations-with-alphabet.cpp
@@ -1,57 +1,41 @@
 #include <iostream>
-#include <vector>
 #include <climits>
+#define MAX_NUM 6
 using namespace std;
-int n,arr[10],cnt=0,maxx=INT_MIN;
+int arr[MAX_NUM];
+int n =6;
 string expression;
-int var[10];
-void func(int a){
-    
-    if(a==cnt){
-        int sum=0;
-        sum+=arr[expression[0]-'a'];
-       
-        for(int i=1;i<n;i=i+2){
-            
-            if(expression[i]=='+'){
-                sum+=arr[expression[i+1]-'a'];
-            }else if(expression[i]=='-'){
-                sum-=arr[expression[i+1]-'a'];
-            }else if(expression[i]=='*'){
-                sum*=arr[expression[i+1]-'a'];
-            }
-            
-            
-        }
-        maxx=max(maxx,sum);
+int maxx=INT_MIN;
+int conv(int idx){
+
+    return arr[expression[idx]-'a']; 
+}
+int cal(){
+    int len=expression.size();
+    int value=0;
+    value+= conv(0);
+    for(int i=2;i<len;i+=2){
+        if(expression[i-1]=='+') value+=conv(i);
+        else if(expression[i-1]=='-')value-=conv(i);
+        else value*=conv(i);
+    }
+    return value;
+}
+void func(int cnt){
+    if(cnt==n){
+        maxx=max(maxx,cal());
         return;
     }
     for(int i=1;i<=4;i++){
-       
-        
-            arr[var[a]]=i;
-            func(a+1);
-          
-       
-     
+        arr[cnt]=i;
+        func(cnt+1);
     }
 }
 int main() {
     cin >> expression;
-    n=expression.length();
-    for(int i=0;i<n;i++){
-        if(expression[i]>='a'&&expression[i]<='f'){
-            arr[expression[i]-'a']++;
-            
-        }
-    }
-    for(int i=0;i<10;i++){
-        if(arr[i]>0){var[cnt++]=i;}
-    }
-    
     func(0);
     cout<<maxx;
     // Please write your code here.
-    
+
     return 0;
 }
